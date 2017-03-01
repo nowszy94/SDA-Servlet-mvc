b0069b798f6bf0c77f729e1162cea296f1ff3fea
nowszy94 <nowszy94@gmail.com>
2017-03-01 21:06:34 +0100

    Added some cookie controller

---
+++ src/main/java/com/szymon/servlet/HelloServlet.java
@@ -0,0 +1,44 @@
+package com.szymon.servlet;
+
+import com.szymon.controller.Controller;
+import com.szymon.controller.CookieController;
+import com.szymon.controller.LoginController;
+import com.szymon.controller.UserController;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HelloServlet extends HttpServlet {
+
+    private Map<String, Controller> controllerMap;
+
+    @Override
+    public void init() throws ServletException {
+        this.controllerMap = new HashMap<String, Controller>();
+        this.controllerMap.put("users", new UserController());
+        this.controllerMap.put("login", new LoginController());
+        this.controllerMap.put("cookie", new CookieController());
+        this.controllerMap.put("default", (request, response) ->
+                response.getWriter().write("<h1>Hello from default controller</h1>"));
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        String requestURI = req.getRequestURI();
+        String relativePath = requestURI.substring(StringUtils.ordinalIndexOf(requestURI, "/", 2) + 1);
+        Controller controller = controllerMap.get(relativePath);
+        if (controller == null) {
+            controller = controllerMap.get("default");
+        }
+        controller.handleGet(req, resp);
+    }
+}
